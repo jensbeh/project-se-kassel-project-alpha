99614429392454318223c0adae11d0bf75782482
Jens Behmenburg <jensbehmenburg@gmail.com>
2022-07-04 19:40:52 +0200

    feat: added real time system

--- autoload/day_night_cycle.gd
+++ autoload/day_night_cycle.gd
@@ -5,6 +5,11 @@ extends Node
 var current_time = 0.0
 var screen_color : Color
 
+# Time
+var time = 0
+var current_hour = 0
+var current_minute = 0
+
 # Constants
 const COMPLETE_DAY_TIME = 1200.0 # 24 std -> 20min = 1200s
 # Times in sum == COMPLETE_DAY_TIME
@@ -13,15 +18,30 @@ const SUNSET_TIME = COMPLETE_DAY_TIME / 12 # 2 std ->  1.666min = 100s
 const NIGHT_TIME = COMPLETE_DAY_TIME / 3 # 8 std ->  6.6666min = 400s
 const SUNRISE_TIME = COMPLETE_DAY_TIME / 12 # 2 std ->  1.666min = 100s
 
+const ONE_HOUR = COMPLETE_DAY_TIME / 24
+const ONE_MINUTE = ONE_HOUR / 60
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	# Calculate hours and minutes from current_time
+	current_hour = int(floor(current_time / ONE_HOUR)) + 8
+	current_minute = int((fmod((current_time / ONE_HOUR), 1) * ONE_HOUR))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	current_time += delta
 #	print(current_time)
+	time += delta
+	if time >= ONE_MINUTE:
+		current_minute += 1
+		time = 0
+		print(str(current_hour) + ":" + str(current_minute))
+	if current_minute == 60:
+		current_minute = 0
+		current_hour += 1
+	if current_hour >= 24:
+		current_hour = 0
 	
 	# Reset timer on new day
 	if current_time > COMPLETE_DAY_TIME:
@@ -56,7 +76,6 @@ func _process(delta):
 	elif current_time <= (DAY_TIME + SUNSET_TIME + NIGHT_TIME + SUNRISE_TIME):
 		var value = (current_time - (DAY_TIME + SUNSET_TIME + NIGHT_TIME + (SUNRISE_TIME / 2))) / (SUNRISE_TIME / 2)
 		screen_color = Constants.SUNRISE_COLOR.linear_interpolate(Constants.DAY_COLOR, value)
-
 
 func get_screen_color():
 	return screen_color
