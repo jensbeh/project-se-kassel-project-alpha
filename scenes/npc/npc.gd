a7bdc6eefc054e537e5e7e491828acb0cddf03cb
Tim <uk063246@student.uni-kassel.de>
2022-07-07 00:09:07 +0200

    feat: added stay function

--- scenes/npc/npc.gd
+++ scenes/npc/npc.gd
@@ -15,6 +15,7 @@ var path_exists
 var target = null
 var time = 0.0
 var turn = randi() % 60
+var stay
 
 onready var animation_tree = $AnimationTree
 onready var animation_player = $AnimationPlayer
@@ -23,8 +24,7 @@ onready var animation_state = animation_tree.get("parameters/playback")
 
 # inventar
 # stehenbleiben -> 50sec = 1h 
-# starre character drehen sich zu player oder random jede stunde mit 30% whrs
-# stehen bleiben / verschwinden -> abhängig von zeit
+# stehen bleiben -> abhängig von zeit
 # sprache
 func _ready():
 	path_exists = (self.get_parent().get_parent().find_node(self.name + "_Path") != null)
@@ -49,8 +49,9 @@ func _physics_process(delta):
 		time = 0
 
 	if path_exists:
+		turn = 50
 		target = patrol_points[patrol_index]
-		if position.distance_to(target) < 1:
+		if position.distance_to(target) < 1 and time < 46:
 			if circle:
 				if patrol_index < patrol_points.size() -1 and !stop:
 					patrol_index = patrol_index + 1
@@ -61,7 +62,7 @@ func _physics_process(delta):
 				if patrol_index < patrol_points.size() -1 and !wayback and !stop:
 					patrol_index = patrol_index + 1
 					target = patrol_points[patrol_index]
-				if !wayback and patrol_index == patrol_points.size() -1:
+				if !wayback and patrol_index == patrol_points.size() -1 and time < 3:
 					wayback = true
 				if patrol_index > 0 and wayback and !stop:
 					patrol_index = patrol_index - 1
@@ -85,11 +86,17 @@ func _physics_process(delta):
 				velocity = Vector2(1,0)
 			if direction == 3:
 				velocity = Vector2(-1,0)
+	
+	if path_exists:
+		if patrol_index == patrol_points.size() -1 and time >= 3 or position.distance_to(target) < 1 and time >= 46:
+			stay = true
+		else:
+			stay = false
 
-	if stop or target == null:
+	if stop or target == null or stay:
 		animation_tree.active = false
 			
-	if velocity != Vector2.ZERO and !stop and target != null:
+	if velocity != Vector2.ZERO and !stop and target != null and !stay:
 		animation_tree.set("parameters/Idle/blend_position", velocity)
 		animation_tree.set("parameters/Walk/blend_position", velocity)
 		animation_state.travel("Walk")
@@ -98,7 +105,7 @@ func _physics_process(delta):
 		animation_tree.set("parameters/Idle/blend_position", velocity)
 		animation_state.travel("Idle")
 
-	if !stop and target != null:
+	if !stop and target != null and !stay:
 		velocity = move_and_slide(velocity)
 
 
@@ -106,12 +113,16 @@ func _on_interactionZone_NPC_body_entered(body):
 	if body == Utils.get_current_player():
 		player_in_interacting_zone = true
 		stop = true
+		if self.visible == false:
+			self.visible = true
 
 
 func _on_interactionZone_NPC_body_exited(body):
 	if body == Utils.get_current_player():
 		player_in_interacting_zone = false
 		stop = false
+		if DayNightCycle.get_current_time() > 1100 and DayNightCycle.get_current_time() < 3000:
+			self.visible = false
 
 
 func interaction_detected():
