40078ebc2cdddfb1b49b159efd0c9140e63fc7b3
Tim <uk063246@student.uni-kassel.de>
2022-08-06 14:15:36 +0200

    fix: close popup while click outside

--- scenes/inventory/item_split_popup.gd
+++ scenes/inventory/item_split_popup.gd
@@ -45,4 +45,8 @@ func _input(event):
 		_on_Confirm_pressed()
 	if event.is_action_pressed("esc"):
 		queue_free()
-
+	# close popup if clicked outside
+	if (event is InputEventMouseButton) and event.pressed:
+		var evLocal = make_input_local(event)
+		if !Rect2(Vector2(0,0),rect_size).has_point(evLocal.position):
+			queue_free()
