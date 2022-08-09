c300b2c5a7e1ad6686b2a7544928960bbb91b669
Tim <uk063246@student.uni-kassel.de>
2022-08-09 18:24:51 +0200

    feat: added compare to the new stats

--- scenes/inventory/tooltip.gd
+++ scenes/inventory/tooltip.gd
@@ -35,9 +35,12 @@ func _ready():
 				else:
 					get_node("NinePatchRect/Margin/VBox/Stats" + str(item_stat) + "/Stat").set_text(tr(stat_label) + ": " + str(stat_value) + " Gold")
 				
-				if GameData.item_data[item_id]["EquipmentSlot"] != null and (origin == "Inventory" or origin == "TradeInventory") and stat_name == "Attack":
+				if GameData.item_data[item_id]["EquipmentSlot"] != null and (origin == "Inventory" or origin == "TradeInventory") and stat_name in GameData.compare_stats:
 					var stat_difference = CompareItems(item_id, stat_name, stat_value)
-					if stat_difference > 0:
+					if stat_name == "Attack-Speed" and stat_difference < 0:
+						get_node("NinePatchRect/Margin/VBox/Stats" + str(item_stat) + "/Difference").set_text(str(stat_difference))
+						get_node("NinePatchRect/Margin/VBox/Stats" + str(item_stat) + "/Difference").set("custom_colors/font_color", Color("3eff00"))
+					elif stat_difference > 0:
 						get_node("NinePatchRect/Margin/VBox/Stats" + str(item_stat) + "/Difference").set_text("+" + str(stat_difference))
 						get_node("NinePatchRect/Margin/VBox/Stats" + str(item_stat) + "/Difference").set("custom_colors/font_color", Color("3eff00"))
 					elif stat_difference < 0:
