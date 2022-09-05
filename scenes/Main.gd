bebcabc1a2b8bbf18ed7d27b8636b38960dce4d6
Jens Behmenburg <jensbehmenburg@gmail.com>
2022-09-05 19:55:52 +0200

    fix: changed some things

--- scenes/Main.gd
+++ scenes/Main.gd
@@ -1,16 +1,23 @@
 extends Node2D
 
 
-onready var viewport1 = $Game/Viewport
-onready var viewport2 = $Minimap/Viewport
+onready var game_viewport_container = $Game
+onready var game_viewport = $Game/Viewport
+onready var minimap_viewport_container = $Minimap
+onready var minimap_viewport = $Minimap/Viewport
 onready var minimap = $Minimap/Viewport/Camera2D
 onready var sc = $Game/Viewport/SceneManager/CurrentScene
 
+
 func _ready():
-	viewport2.world_2d = viewport1.world_2d
-	minimap.zoom = Vector2(1, 1)
+	# Set world / what game_viewport sees to minimap_viewport
+	minimap_viewport.world_2d = game_viewport.world_2d
+	
+	# Set zoom factor for minimap
+	minimap.zoom = Vector2(1.5, 1.5) # min: 1; max: 1.5
 
 
 func _physics_process(_delta):
+	# Move map position with player position
 	if Utils.get_current_player() != null:
 		minimap.position = Utils.get_current_player().global_position
