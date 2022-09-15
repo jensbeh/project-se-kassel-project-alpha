f59d0163d7800a5768afbb467683f5341f7e6849
Jens Behmenburg <jensbehmenburg@gmail.com>
2022-09-15 14:44:33 +0200

    feat: added animation to boss health bar when die

--- scenes/mobs/bosses/boss_fungus.gd
+++ scenes/mobs/bosses/boss_fungus.gd
@@ -11,6 +11,7 @@ var previouse_player_global_position
 var previouse_global_position
 var is_attacking = false
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Setup mob
