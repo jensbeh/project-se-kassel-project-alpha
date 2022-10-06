95e6fa2c6688fc13f26fd667fe99ee706160845c
Tim <uk063246@student.uni-kassel.de>
2022-10-06 14:38:29 +0200

    set spawn to camp back

--- scenes/menu/character_screens/character_screen.gd
+++ scenes/menu/character_screens/character_screen.gd
@@ -528,11 +528,11 @@ func start_game():
 	Utils.set_current_player(Utils.get_player())
 
 	# Set spawn
-#	var player_position = Vector2(1128,616) # Camp
+	var player_position = Vector2(1128,616) # Camp
 #	var player_position = Vector2(768,752) # Grassland - Dungeon1
 #	var player_position = Vector2(-816,-496) # Grassland - Mountain
 #	var player_position = Vector2(336,-62) # Dungeon1-1
-	var player_position = Vector2(784,16) # Dungeon1-3
+#	var player_position = Vector2(784,16) # Dungeon1-3
 #	var player_position = Vector2(240,480) # Dungeon2-4
 #	var player_position = Vector2(-384,176) # Dungeon3-4
 	var view_direction = Vector2(0,1)
@@ -559,10 +559,10 @@ func start_game():
 		Utils.get_hotbar().set_cooldown(data.cooldown)
 	
 	# Transition
-#	var transition_data = TransitionData.GamePosition.new(Constants.CAMP_FOLDER + "/Camp.tscn", player_position, view_direction)
+	var transition_data = TransitionData.GamePosition.new(Constants.CAMP_FOLDER + "/Camp.tscn", player_position, view_direction)
 #	var transition_data = TransitionData.GamePosition.new(Constants.GRASSLAND_SCENE_PATH, player_position, view_direction)
 #	var transition_data = TransitionData.GamePosition.new("res://scenes/dungeons/dungeon1/Dungeon1-lvl1.tscn", player_position, view_direction)
-	var transition_data = TransitionData.GamePosition.new("res://scenes/dungeons/dungeon1/Dungeon1-lvl3.tscn", player_position, view_direction)
+#	var transition_data = TransitionData.GamePosition.new("res://scenes/dungeons/dungeon1/Dungeon1-lvl3.tscn", player_position, view_direction)
 #	var transition_data = TransitionData.GamePosition.new("res://scenes/dungeons/dungeon2/Dungeon2-lvl4.tscn", player_position, view_direction)
 #	var transition_data = TransitionData.GamePosition.new("res://scenes/dungeons/dungeon3/Dungeon3-lvl4.tscn", player_position, view_direction)
 	Utils.get_scene_manager().transition_to_scene(transition_data)
