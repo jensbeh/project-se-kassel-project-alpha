5bc886b69a35e3c96aa24c6f90f24800f9021f77
Jens Behmenburg <jensbehmenburg@gmail.com>
2022-10-31 13:40:24 +0100

    feat: added tilesets

---
+++ assets/tilesets/cabin_animated_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="cabin_animated_tiles" tilewidth="16" tileheight="16" tilecount="24" columns="4">
+ <image source="Cabin Animated Tiles 1.0.png" width="64" height="96"/>
+</tileset>
---
+++ assets/tilesets/quiet_cabin_furniture.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="quiet_cabin_furniture" tilewidth="16" tileheight="16" tilecount="300" columns="25">
+ <image source="QuietCabinFurniture.png" width="400" height="192"/>
+</tileset>
