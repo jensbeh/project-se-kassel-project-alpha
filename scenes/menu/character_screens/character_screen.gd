ca39f5c5715cd71d164fe332a8043554b9d8d812
Jens Behmenburg <jensbehmenburg@gmail.com>
2022-11-21 18:49:39 +0100

    fix: load player spawn

--- scenes/menu/character_screens/character_screen.gd
+++ scenes/menu/character_screens/character_screen.gd
@@ -595,7 +595,7 @@ func start_game():
 			Utils.get_hotbar().set_cooldown_health(data.cooldown, "Health")
 		
 	# Set spawn
-	var player_position = Vector2(1128,616) # Camp
+#	var player_position = Vector2(1128,616) # Camp
 #	var player_position = Vector2(768,752) # Grassland - Dungeon1
 #	var player_position = Vector2(1056,-80) # Grassland - Beach
 #	var player_position = Vector2(1040, 64) # Grassland - Dungeon1/Beach
@@ -608,10 +608,10 @@ func start_game():
 #	var player_position = Vector2(240,480) # Dungeon2-4
 #	var player_position = Vector2(-300,64) # Dungeon3-2
 #	var player_position = Vector2(-384,176) # Dungeon3-4
-	var view_direction = Vector2(0,1)
+#	var view_direction = Vector2(0,1)
 	
 	# Transition
-	var transition_data = TransitionData.GamePosition.new(Constants.CAMP_FOLDER + "/Camp.tscn", player_position, view_direction)
+#	var transition_data = TransitionData.GamePosition.new(Constants.CAMP_FOLDER + "/Camp.tscn", player_position, view_direction)
 #	var transition_data = TransitionData.GamePosition.new(Constants.GRASSLAND_SCENE_PATH, player_position, view_direction)
 #	var transition_data = TransitionData.GamePosition.new("res://scenes/dungeons/dungeon1/Dungeon1-lvl1.tscn", player_position, view_direction)
 #	var transition_data = TransitionData.GamePosition.new("res://scenes/dungeons/dungeon1/Dungeon1-lvl3.tscn", player_position, view_direction)
@@ -620,9 +620,9 @@ func start_game():
 #	var transition_data = TransitionData.GamePosition.new("res://scenes/dungeons/dungeon3/Dungeon3-lvl4.tscn", player_position, view_direction)
 	
 	# Set spawn & transition
-#	var player_position = str2var(data.position)
-#	var view_direction = str2var(data.view_direction)
-#	var transition_data = TransitionData.GamePosition.new(data.scene_transition, player_position, view_direction)
+	var player_position = str2var(data.position)
+	var view_direction = str2var(data.view_direction)
+	var transition_data = TransitionData.GamePosition.new(data.scene_transition, player_position, view_direction)
 	
 	Utils.get_scene_manager().transition_to_scene(transition_data)
 
